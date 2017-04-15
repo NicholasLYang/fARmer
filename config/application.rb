1aead65b9239b775d2a83065f5beb692b6b2f049
Nicholas Yang <nick@nicholasyang.com>
2017-04-15 17:56:33 -0400

    config

--- config/application.rb
+++ config/application.rb
@@ -15,6 +15,6 @@ module FARmer
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
 
-    config.web_console.whitelisted_ips = '66.251.27.35'
+
   end
 end
